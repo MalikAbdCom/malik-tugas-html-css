aa24eb9dca5d291dd0cb3abac864c3f639c1cb69
Malik Abdullah <emailmalikabdcom@gmail.com>
2022-06-01 06:53:35 +0700

    fixing hamburger icon-mobile

--- js/main.js
+++ js/main.js
@@ -10,16 +10,20 @@ function toggleNav() {
   mobileNavToggle.classList.toggle('nav-mobile_toggle');
   borderRadiusNav.classList.toggle("nav-sticky_bot-clear")
 }
-//                  end of function
-getHamburgerBtn.addEventListener('click', toggleNav);
-//===
-getHamburgerBtn.addEventListener("click", function () {
+function hamburgerToggle() {
   //styling hamburger
   hamburgerIcon[0].classList.toggle('nav-hamburger_clicked-top')
   hamburgerIcon[1].classList.toggle('nav-hamburger_clicked-middle')
   hamburgerIcon[2].classList.toggle('nav-hamburger_clicked-bottom')
-})
+}
+//                  end of function
+getHamburgerBtn.addEventListener('click', toggleNav);
+//===
+getHamburgerBtn.addEventListener("click", hamburgerToggle)
 
 navMenuList.forEach(function (e) {
-  e.addEventListener("click", toggleNav)
+  e.addEventListener("click", function () {
+    toggleNav()
+    hamburgerToggle()
+  })
 })
