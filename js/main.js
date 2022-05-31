77e53ab4f631eb3c22bb72150eba720a3e1e4cb9
Malik Abdullah <emailmalikabdcom@gmail.com>
2022-05-31 21:56:16 +0700

    done

--- js/main.js
+++ js/main.js
@@ -2,6 +2,8 @@ const getHamburgerBtn = document.querySelector('.nav-button_hamburger');
 const borderRadiusNav = document.querySelector('.nav-sticky');
 const mobileNavToggle = document.querySelector(".nav-mobile_open")
 const navMenuList = document.querySelectorAll(".nav-mobile_open a")
+const hamburgerIcon = document.querySelectorAll(".nav-hamburger_button-line")
+
 //                  end of variable
 function toggleNav() {
   borderRadiusNav.classList.toggle('nav-sticky_open');
@@ -10,6 +12,13 @@ function toggleNav() {
 }
 //                  end of function
 getHamburgerBtn.addEventListener('click', toggleNav);
+//===
+getHamburgerBtn.addEventListener("click", function () {
+  //styling hamburger
+  hamburgerIcon[0].classList.toggle('nav-hamburger_clicked-top')
+  hamburgerIcon[1].classList.toggle('nav-hamburger_clicked-middle')
+  hamburgerIcon[2].classList.toggle('nav-hamburger_clicked-bottom')
+})
 
 navMenuList.forEach(function (e) {
   e.addEventListener("click", toggleNav)
